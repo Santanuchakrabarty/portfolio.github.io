f26c6febfa305cbc267b2d4341fb07c634b651d2
Santanu Chakrabarty <45288833+Santanuchakrabarty@users.noreply.github.com>
2023-02-25 14:28:46 +0530

    xx
    sas

--- js/textRotate.js
+++ js/textRotate.js
@@ -9,7 +9,7 @@ var theForm = document.getElementById( 'theForm' );
           classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
           //console.log("Submitted Request");
 
-          var link="https://docs.google.com/forms/d/e/1FAIpQLSd1JpwNaH-iBJ80_9PvKD3zPXbIGor10O6msHvIIP1MRgfbcw/formResponse";
+          var link="https://docs.google.com/forms/d/1_dJLJE7kM3BOSukmMmHOHA9kMDMd0KPbk72NWQje6QU/prefill";
           jQuery.ajax({
                 url: link,
                 data: {"entry.2111170142" : $("#q2").val() , "entry.2040235095" : $("#q3").val() , "entry.72030663" : $("#q1").val() },
